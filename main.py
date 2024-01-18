0dbba324f24241b82b457fdca9e4e8c7e6daf343
harinduj93 <harindu101@gmail.com>
2024-01-18 15:10:19 +0200

    update

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
 from xml.etree import ElementTree as ET
 
+
+
 def calculate_total_size(xml_file):
     # Parse the XML file
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
     total_size = 0
+    current_url_number = 1
 
     # Define the namespace
     ns = {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
@@ -17,12 +20,16 @@ def calculate_total_size(xml_file):
         file_size = get_file_size(file_url)
         print(f'File URL: {file_url}, File Size: {file_size} bytes')
         total_size += file_size
+        current_url_number += 1
+        print(f'total fils size current {total_size} : bytes')
+        print(f'Total file size: {total_size / (1024 * 1024):.2f} MB')
+        print(f'Finished calculating: {current_url_number} of {root.__sizeof__()}')
 
     return total_size
 
 def get_file_size(url):
     # Make a HEAD request to get file size without downloading the file
-    response = requests.head(url)
+    response = requests.head(url, timeout=10)
 
     # Check if the HEAD request was successful (status code 200)
     if response.status_code == 200:
@@ -72,5 +79,7 @@ def download_file(url):
         print(f'Failed to download: {url}, Status code: {response.status_code}')
 
 # Example usage
+
+
 xml_file_path = 'sitemap_manuale.xml'
 download_files_from_xml(xml_file_path)
